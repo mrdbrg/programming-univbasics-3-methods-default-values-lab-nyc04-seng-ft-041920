8104136e121b991ebd120457509ec5ecb29513de
Marlon Braga <rdg.brg@gmail.com>
2020-04-23 00:30:55 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -6,5 +6,5 @@ def meal_choice(veg1, veg2, protein = "tofu")
   return "A plate of #{protein} with #{veg1} and #{veg2}."
 end
 
-meal_choice('carrots', 'string beans', 'lentils')
-meal_choice('carrots', 'string beans')
+puts meal_choice('carrots', 'string beans', 'lentils')
+puts meal_choice('carrots', 'string beans')
